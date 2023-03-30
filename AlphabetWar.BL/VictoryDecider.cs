5cd45f0ad2f05b7b0d8ca1ab6a347d7f57282031
jesse <jessefortuin850@gmail.com>
2023-03-30 19:05:42 +0200

    Feat: method implemented

--- AlphabetWar.BL/VictoryDecider.cs
+++ AlphabetWar.BL/VictoryDecider.cs
@@ -4,11 +4,52 @@
     {
         public string AlphabetWar(string fight)
         {
+            int right = 0;
 
+            int left = 0;
 
+            foreach (char c in fight) 
+            {
+                switch (c)
+                {
+                    case 'm': right += 4; 
+                        break;
+                        
+                    case 'q': right += 3; 
+                        break;
+                        
+                    case 'd': right += 2; 
+                        break;
+                        
+                    case 'z': right += 1; 
+                        break;
 
+                    case 'w': left += 4;
+                        break;  
+                          
+                    case 'p': left += 3;
+                        break;  
+                          
+                    case 'b': left += 2;
+                        break;  
+                          
+                    case 's': left += 1;
+                        break;   
+    
+                }
+            }
 
-            return fight;
+            if (right > left) 
+            {
+                return "Right side wins!";
+            }
+
+            else if (left > right) 
+            {
+                return "Left side wins!";
+            }
+
+            return "Let's fight again!";
         }
     }
 }
--- AlphabetWar.Test/VictoryDeciderTest.cs
+++ AlphabetWar.Test/VictoryDeciderTest.cs
@@ -1,11 +1,24 @@
+using AlphabetWar.BL;
+
 namespace AlphabetWar.Test
 {
     public class VictoryDeciderTest
     {
-        [Fact]
-        public void AlphabetWarTest()
+        [Theory]
+        [InlineData("z", "Right side wins!")]
+        [InlineData("zdqmwpbs", "Let's fight again!")]
+        [InlineData("zzzzs", "Right side wins!")]
+        [InlineData("wwwwwwz", "Left side wins!")]
+        public void AlphabetWarTest(string input, string output)
         {
+            //arrange
+            IVictoryDecider decider = new VictoryDecider();
 
+            //act
+            string actual = decider.AlphabetWar(input);
+
+            //assert
+            Assert.Equal(output, actual);
         }
     }
 }
